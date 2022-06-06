cf775d3f177d3d3fbd0955ee9aa768343321c6a5
CrossStack-Q <crossstack@gmail.com>
2022-06-06 10:19:29 -0700

    6 June Spotify APP

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: 'playlistState',
+    default: null,
+  });
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: '5ppJJxxAR49RdiDWaklHwB?si=a53f1eb9adfc497f'
+})
